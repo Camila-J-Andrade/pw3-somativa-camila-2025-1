7ce6a51941b60c45acc0b8df97c937169537c246
Camila <camilajandrade.11@gmail.com>
2025-02-20 22:27:34 -0300

    Início da barra de navegação

---
+++ pw3-somativa-camila-2025-1/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+import NavBar from './components/layout/NavBar'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <NavBar />
+      </div>
+    </>
+  )
+}
+
+export default App
