f82ac66bb4e8f13a987b6f0987f9784d2e2c8324
Camila <camilajandrade.11@gmail.com>
2025-03-06 17:23:13 -0300

    roteamento da navbar

--- pw3-somativa-camila-2025-1/src/App.jsx
+++ pw3-somativa-camila-2025-1/src/App.jsx
@@ -1,16 +1,52 @@
-import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import NavBar from './components/layout/NavBar'
+import './App.css';
+
+import Container from './components/layout/container';
+
+/* IMPORTS DOS COMPONENTS DE PÁGINAS */
+import Home from './components/Pages/Home';
+import CreateBook from './components/Pages/CreateBook';
+import ListBook from './components/Pages/ListBook';
+
+/* IMPORTAÇÃO DO NAVBAR */
+import NavBar from './components/layout/NavBar';
 
 function App() {
 
   return (
     <>
       <div>
-        <NavBar />
+
+        <BrowserRouter>
+
+          <Container>
+
+            <Routes>
+
+              <Route path='/' element={<NavBar />}>
+
+                <Route path='/' element={<Home />} />
+                
+                <Route path='/newBook' element={<CreateBook />} />
+                
+                <Route path='/listBook' element={<ListBook />} />
+                
+              </Route>
+
+            </Routes>
+            
+            {/* <CreateBook />
+            <ListBook />
+            <Home /> */}
+
+          </Container>
+
+        </BrowserRouter>
+
       </div>
     </>
   )
 }
 
-export default App
+export default App;
